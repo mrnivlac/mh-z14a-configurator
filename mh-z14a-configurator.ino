f9bbaf9ec8d2b2d0698f9d777fb4e7b297b881fb
calvin <rupcal@gmail.com>
2021-06-15 14:42:37 +0800

    added readme

--- mh-z14a-configurator.ino
+++ mh-z14a-configurator.ino
@@ -1,30 +1,3 @@
-/*
-  Software serial multple serial test
-
- Receives from the hardware serial, sends to software serial.
- Receives from software serial, sends to hardware serial.
-
- The circuit:
- * RX is digital pin 10 (connect to TX of other device)
- * TX is digital pin 11 (connect to RX of other device)
-
- Note:
- Not all pins on the Mega and Mega 2560 support change interrupts,
- so only the following can be used for RX:
- 10, 11, 12, 13, 50, 51, 52, 53, 62, 63, 64, 65, 66, 67, 68, 69
-
- Not all pins on the Leonardo and Micro support change interrupts,
- so only the following can be used for RX:
- 8, 9, 10, 11, 14 (MISO), 15 (SCK), 16 (MOSI).
-
- created back in the mists of time
- modified 25 May 2012
- by Tom Igoe
- based on Mikal Hart's example
-
- This example code is in the public domain.
-
- */
 #include <LiquidCrystal_I2C.h>
 #include <SoftwareSerial.h>
 #include <Wire.h>
@@ -32,9 +5,8 @@
 const int pinCal = 2;
 
 SoftwareSerial mSerial(7, 6);  // RX, TX
-LiquidCrystal_I2C lcd(
-    0x27, 20,
-    4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+// set the LCD address to 0x27 for a 16 chars and 2 line display
 
 const byte setRange[] = {0xff, 0x01, 0x99, 0,   0,
                          0,    0x13, 0x88, 0xcb};  // 5000ppm
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Parts
+* Arduino duemuilanove
+* 20x4 LCD with I2C expander
+* MH-Z14A CO2 Sensor
+
+# Connections
+Port | Purpose
+---|---
+I2C | LCD
+A0 | Sample MH-Z14A Analog output
+D7,D6 | Softserial MH-Z14A commands
+D2 | Button to trigger calibration
+RESET | Button to reset board
+
